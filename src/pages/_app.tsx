b88f030935aa7317d5a3b6eaacc4d462ce2999e6
Rafael Santana <rafaelsantana7213@gmail.com>
2023-03-10 11:29:24 -0300

    test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,23 +5,28 @@ import { useEffect } from 'react';
 import { NextIntlProvider } from 'next-intl';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { isReady } = useRouter();
+
   useEffect(() => {
-    if (typeof window !== 'undefined') {
+    if (typeof window !== 'undefined' && isReady) {
+      console.log('isReady', isReady);
+      console.log('window', window?.M);
       const carousel = document.querySelectorAll('.carousel');
       const tooltip = document.querySelectorAll('.tooltipped');
       const changeColor = document?.getElementById?.('color');
-      if (carousel) {
+      if (carousel && (window as any)?.M) {
         (window as any).M.Carousel.init(carousel);
       }
-      if (tooltip) {
+      if (tooltip && (window as any)?.M) {
         (window as any).M.Tooltip.init(tooltip);
       }
 
       if (changeColor) changeColor.style.display = 'flex';
     }
-  }, []);
+  }, [isReady]);
 
   return (
     <NextIntlProvider messages={pageProps.messages}>
